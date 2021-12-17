63531348b5e0018513cb14a45c7772211dfe5c80
mabersold <mabersold@egencia.com>
2021-12-17 13:28:16 -0800

    Removing local db info from test settings

--- modarchive/settings/test.py
+++ modarchive/settings/test.py
@@ -17,19 +17,5 @@ MIDDLEWARE = list(MIDDLEWARE) + [
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': 'localhost',
-        'NAME': 'mod_archive',
-        'USER': 'postgres',
-        'PASSWORD': 'docker',
-        'PORT': '5432'
-    }
-}
-
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
