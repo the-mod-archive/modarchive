01c4434873e0e9aeb14ba62202413610cc27267a
mabersold <mabersold@yahoo.com>
2022-02-18 19:07:11 -0800

    Added artist search

--- artists/models.py
+++ artists/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 from songs.models import Song
+from django.contrib.postgres.search import SearchVectorField
+from django.contrib.postgres.indexes import GinIndex
 
 class Artist(models.Model):
     legacy_id=models.IntegerField(null=True)
     key=models.CharField(max_length=32, db_index=True)
     name=models.CharField(max_length=64)
     songs=models.ManyToManyField(Song)
+    search_document=SearchVectorField(null=True)
     create_date=models.DateTimeField(auto_now_add=True)
     update_date=models.DateTimeField(auto_now=True)
+
+    class Meta:
+        indexes = [
+            GinIndex(fields=['search_document'])
+        ]
+
+    def index_components(self):
+        return {
+            'A': self.name
+        }
--- search/views.py
+++ search/views.py
@@ -1,19 +1,49 @@
-from django.db.models import F
+from django.db.models import F, Value, CharField
 from django.shortcuts import render
-from django.contrib.postgres.search import SearchQuery, SearchRank
+from django.contrib.postgres.search import SearchRank
 
+from artists.models import Artist
 from songs.models import Song
 
 def search(request):
     query = request.GET.get('q')
     if query:
-        # Search songs
-        search_results = Song.objects.filter(
-            search_document=SearchQuery(query)
-        ).annotate(
-            rank=SearchRank(F('search_document'), query)
-        ).order_by('-rank')
+        rank_annotation = SearchRank(F('search_document'), query)
+        songs_query_set = Song.objects.annotate(
+            type=Value('song', output_field=CharField()),
+            rank=rank_annotation
+        ).filter(
+            search_document=query
+        ).values('pk', 'type', 'rank')
+
+        artists_query_set = Artist.objects.annotate(
+            type=Value('artist', output_field=CharField()),
+            rank=rank_annotation
+        ).filter(
+            search_document=query
+        ).values('pk', 'type', 'rank')
+
+        merged_query_set = songs_query_set.union(artists_query_set).order_by('-rank')
+
+        to_fetch = {}
+        fetched = {}
+        
+        for d in merged_query_set:
+            to_fetch.setdefault(d['type'], set()).add(d['pk'])
+
+        for key, model in (('song', Song), ('artist', Artist)):
+            ids = to_fetch.get(key) or []
+            objects = model.objects.filter(pk__in=ids)
+            for obj in objects:
+                fetched[(key, obj.pk)] = obj
+
+        final_results = []
+        for d in merged_query_set:
+            item = fetched.get((d['type'], d['pk'])) or None
+            if item:
+                item.original_dict = d
+            final_results.append({'type': d['type'], 'item': item}) 
 
         return render(request, 'search_results.html', {
-            'search_results': search_results
+            'search_results': final_results
         })
