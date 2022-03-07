a75c1dea3c15107e137e8a55620fa4d33023102d
mabersold <mabersold@egencia.com>
2022-03-07 15:33:20 -0800

    Created basic artist tests

--- artists/tests.py
+++ artists/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.urls.base import reverse
 
-# Create your tests here.
+class ArtistViewTests(TestCase):
+    fixtures = ["artists.json", "songs.json"]
+
+    def test_artist_list_view_contains_all_artists(self):
+        # Arrange
+        expected_length = 15
+        
+        # Act
+        response = self.client.get(reverse('artists'))
+        
+        # Assert
+        actual_length = len(response.context['object_list'])
+        
+        self.assertTemplateUsed(response, 'artist_list.html')
+        self.assertTrue('object_list' in response.context)
+        self.assertEqual(expected_length, actual_length, f"Expected {expected_length} objects in artists list but got {actual_length}")
+
+    def test_artist_view_contains_specific_artist(self):
+        # Act
+        response = self.client.get(reverse('view_artist', kwargs = {'key': 'Arcturus'}))
+
+        # Assert
+        self.assertTemplateUsed(response, 'artist.html')
+        self.assertTrue('artist' in response.context)
+
+        artist = response.context['artist']
+        self.assertEquals('Arcturus', artist.name)
+        self.assertEquals(69117, artist.legacy_id)
