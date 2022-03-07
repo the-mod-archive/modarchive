d24a3c8a3279e1aa6093734d72da871fec850808
mabersold <mabersold@egencia.com>
2022-03-07 12:26:30 -0800

    Added tests, and some instructions for testing

--- Readme.md
+++ Readme.md
@@ -14,3 +14,12 @@ First, you will need Python and Django, naturally. You will also need to have a 
 
 ### Execute
 
+## Setting up testing in local environment
+
+The following instructions are relevant for use with Visual Studio Code. Please note that every directory with tests will need to have __init__.py in the directory.
+
+First, do pip install pytest-django.
+
+Second, in Visual Studio Code, click on "Testing" and select pytest as your testing framework.
+
+The pytest.ini that is included in this repository should allow VS code to discover all the tests available. Note that you will need your database instance to be running to execute the tests.
--- songs/tests.py
+++ songs/tests.py
@@ -1,5 +1,5 @@
-from turtle import title
 from django.test import TestCase
+from django.urls.base import reverse
 
 from songs.models import Song
 
@@ -11,3 +11,32 @@ class SongTests(TestCase):
     def test_gets_original_title_when_no_clean_title_available(self):
         song = Song(title="song title")
         self.assertEqual("song title", song.get_title())
+
+class SongViewTests(TestCase):
+    fixtures = ["songs.json"]
+
+    def test_song_list_view_contains_all_songs(self):
+        # Arrange
+        expected_length = 33
+        
+        # Act
+        response = self.client.get(reverse('songs'))
+        
+        # Assert
+        actual_length = len(response.context['object_list'])
+        
+        self.assertTemplateUsed(response, 'song_list.html')
+        self.assertTrue('object_list' in response.context)
+        self.assertEqual(expected_length, actual_length, f"Expected {expected_length} objects in songs list but got {actual_length}")
+
+    def test_song_view_contains_specific_song(self):
+        # Act
+        response = self.client.get(reverse('view_song', kwargs = {'pk': 1}))
+
+        # Assert
+        self.assertTemplateUsed(response, 'song.html')
+        self.assertTrue('song' in response.context)
+
+        song = response.context['song']
+        self.assertEquals('Tangerine Fascination', song.get_title())
+        self.assertEquals(48552, song.legacy_id)
