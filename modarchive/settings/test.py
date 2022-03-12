25e4496797aacf1c4ee14a6afb041fa9b14e0b44
mabersold <mabersold@yahoo.com>
2022-03-11 20:17:43 -0800

    Changing test env to use mailgun instead of GMail

--- modarchive/settings/test.py
+++ modarchive/settings/test.py
@@ -26,8 +26,8 @@ DATABASES['default'].update(db_from_env)
 
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv('EMAIL_HOST', '')
-EMAIL_PORT = 587
+EMAIL_HOST = os.getenv('MAILGUN_SMTP_SERVER', '')
+EMAIL_PORT = os.getenv('MAILGUN_SMTP_PORT', '')
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+EMAIL_HOST_USER = os.getenv('MAILGUN_SMTP_LOGIN', '')
+EMAIL_HOST_PASSWORD = os.getenv('MAILGUN_SMTP_PASSWORD', '')
