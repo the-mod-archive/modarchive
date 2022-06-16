996f9e46e2e20c52644e8bfde23dfdc27c6c8dac
mabersold <mabersold@yahoo.com>
2022-06-15 20:43:22 -0700

    Improving many-to-many relationship in admin UI

--- artists/admin.py
+++ artists/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from artists.models import Artist
+from songs.admin import ArtistInline
 
 @admin.register(Artist)
 class ArtistAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name')
-    fields = ('name', 'legacy_id', 'songs')
+    fields = ('name', 'legacy_id')
+    inlines = (ArtistInline,)
     exclude = ('search_document', 'user', 'profile', 'key')
+    search_fields = ['name']
--- songs/admin.py
+++ songs/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from songs.models import Song
+from artists.models import Artist
+
+class ArtistInline(admin.TabularInline):
+    model = Artist.songs.through
 
 @admin.register(Song)
 class SongAdmin(admin.ModelAdmin):
     list_display = ("pk", "title", "clean_title", "filename")
     search_fields = ("title__startswith", )
+    exclude = ("search_document",)
+    inlines = [ArtistInline]
