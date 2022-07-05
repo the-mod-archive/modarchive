216a5b9b7b07faf17ed739e02f1aaae95d87f04d
mabersold <mabersold@egencia.com>
2022-07-05 11:15:34 -0700

    Improving HTML titles
    Also removing unused template and URL

--- songs/urls.py
+++ songs/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path('', ListView.as_view(template_name='song_list.html', model=Song), {}, 'songs'),
     path('<int:pk>/', SongView.as_view(), {}, 'view_song'),
     path('<int:pk>/comment', AddCommentView.as_view(), {}, 'add_comment'),
-    path('<int:pk>/download', download, name='download_song'),
-    path('comment_rejected/', TemplateView.as_view(template_name="comment_rejected.html"), {}, 'comment_rejected')
+    path('<int:pk>/download', download, name='download_song')
 ]
