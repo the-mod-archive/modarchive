a64f9fab79d45321d4767c248dd4c54533fb225c
Mark Abersold <mabersold@yahoo.com>
2022-08-26 16:17:27 -0700

    Player: Clean up interface, make public

--- songs/static/player.js
+++ songs/static/player.js
@@ -3,13 +3,10 @@ window['libopenmpt'] = {};
 function initialize(legacy_id, filename) {
     libopenmpt.onRuntimeInitialized = _ => {
         const player = new ChiptuneJsPlayer(new ChiptuneJsConfig(-1));
-        document.querySelector("#play-button").addEventListener("click", play);
         const path = `https://api.modarchive.org/downloads.php?moduleid=${legacy_id}#${filename}`;
 
-        function play() {
-            document.getElementById('loading-message').classList.remove('is-hidden');
-            player.load(path, afterLoad.bind(this), loadProgress);
-        }
+        document.getElementById('loading-message').classList.remove('is-hidden');
+        player.load(path, afterLoad.bind(this), loadProgress);        
 
         function togglePause() {
             player.togglePause();
@@ -27,7 +24,6 @@ function initialize(legacy_id, filename) {
             document.getElementById('loading-message').classList.add('is-hidden');
             document.getElementById('play-button-icon').classList.remove('fa-play');
             document.getElementById('play-button-icon').classList.add('fa-pause');
-            document.querySelector("#play-button").removeEventListener("click", play);
             document.querySelector("#play-button").addEventListener("click", togglePause);
 
             document.getElementById('seekbar').addEventListener("change", seek, false);
@@ -69,7 +65,7 @@ function initialize(legacy_id, filename) {
         function updateDuration() {
             const sec_num = player.duration();
             const minutes = Math.floor(sec_num / 60);
-            const seconds = Math.floor(sec_num % 60);
+            let seconds = Math.floor(sec_num % 60);
             if (seconds < 10) { seconds = '0' + seconds; }
             if (document.getElementById('song-data').classList.contains('is-hidden')) {
                 document.getElementById('song-data').classList.remove('is-hidden');
