ef621a6b70410b8ad2659d497ddc98cbc78c4400
mabersold <mabersold@yahoo.com>
2022-09-17 21:28:56 -0700

    Committing genres migrations

--- manage.py
+++ manage.py
@@ -3,14 +3,9 @@
 import os
 import sys
 
-# Username: test_admin
-# Password: Z*******1
-
-# Username: superuser
-# Password: Z*******1
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'modarchive.settings.dev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'modarchive.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
