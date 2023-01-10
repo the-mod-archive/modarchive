46997b6d6c53eb98f02caa8d6000c4b37e56f78b
mabersold <mabersold@yahoo.com>
2023-01-09 20:52:59 -0800

    Separating tabs into different views
    Tabs on artist and profile pages are all in separate views now, temporarily disabling the Javascript - plan to replace the Javascript most likely after launch

--- artists/urls.py
+++ artists/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.views.generic import ListView
 
-from artists.models import Artist
-from artists.views.artist_view import ArtistView, ArtistListView
+from artists.views import artist_view
 
 urlpatterns = [
-    path('', ArtistListView.as_view(), {}, 'artists'),
-    path('<int:pk>/', ArtistView.as_view(), {}, 'view_artist')
+    path('', artist_view.ArtistListView.as_view(), {}, 'artists'),
+    path('<int:pk>/', artist_view.ArtistDetailView.as_view(), {}, 'view_artist'),
+    path('<int:pk>/songs', artist_view.ArtistSongsView.as_view(), {}, 'view_artist_songs'),
+    path('<int:pk>/comments', artist_view.ArtistCommentsView.as_view(), {}, 'view_artist_comments'),
+    path('<int:pk>/favorites', artist_view.ArtistFavoritesView.as_view(), {}, 'view_artist_favorites'),
 ]
--- artists/views/artist_view.py
+++ artists/views/artist_view.py
@@ -6,6 +6,22 @@ class ArtistView(DetailView):
     model = Artist
     template_name = 'artist.html'
 
+class ArtistDetailView(DetailView):
+    model = Artist
+    template_name = "artist_overview.html"
+
+class ArtistSongsView(DetailView):
+    model = Artist
+    template_name = "artist_songs.html"
+
+class ArtistFavoritesView(DetailView):
+    model = Artist
+    template_name = "artist_favorites.html"
+
+class ArtistCommentsView(DetailView):
+    model = Artist
+    template_name = "artist_comments.html"
+
 class ArtistListView(ListView):
     model = Artist
     template_name='artist_list.html'
