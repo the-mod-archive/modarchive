28b12671da911e13e0f2c57adcfab68edef49255
mabersold <mabersold@yahoo.com>
2025-05-02 10:40:50 -0700

    Adding more config values to scenesat

--- modarchive/settings/scenesat.py
+++ modarchive/settings/scenesat.py
@@ -15,6 +15,10 @@ ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MAIN_ARCHIVE_DIR = os.getenv('MAIN_ARCHIVE_DIR')
+TEMP_UPLOAD_DIR = os.getenv('TEMP_UPLOAD_DIR')
+NEW_FILE_DIR = os.getenv('NEW_FILE_DIR')
+REJECTED_FILE_DIR = os.getenv('REJECTED_FILE_DIR')
+REMOVED_FILE_DIR = os.getenv('REMOVED_FILE_DIR')
 
 LOGGING = {
     "version": 1,
