8fcfd8dafdb4cc7e5c86a15ec7dc306494cea40d
mabersold <1114721+mabersold@users.noreply.github.com>
2026-05-08 21:56:01 -0700

    API updates

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import SongViewSet, ArtistViewSet, SongSearchAPIView, SongDownloadView
+from .views import SongViewSet, ArtistViewSet, SongSearchAPIView, SongDownloadView, GenreListAPIView, ArtistSearchAPIView
 
 router = DefaultRouter()
 router.register(r'songs', SongViewSet)
@@ -9,5 +9,7 @@ router.register(r'artists', ArtistViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('search', SongSearchAPIView.as_view()),
+    path('genres', GenreListAPIView.as_view()),
+    path('artist_search', ArtistSearchAPIView.as_view()),
     path('download/<int:pk>', SongDownloadView.as_view(), name='song_download'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,16 +1,15 @@
 import os
-import requests
 from rest_framework import viewsets, generics, filters, pagination
 from rest_framework.exceptions import ValidationError
-from django.contrib.postgres.search import SearchVector, SearchQuery
-from django.db.models import F
+from rest_framework.response import Response
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.db.models import F, Q
 from django.views import View
-from django.conf import settings
-from django.http import HttpResponse, Http404, StreamingHttpResponse
+from django.http import HttpResponse, Http404
 
 from songs.models import Song
 from artists.models import Artist
-from .serializers import SongSerializer, ArtistSerializer, SongSearchResultSerializer
+from .serializers import SongSerializer, ArtistSerializer, SongSearchResultSerializer, GenreSerializer, ArtistSearchResultSerializer
 
 class SongViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Song.objects.all()
@@ -21,7 +20,7 @@ class ArtistViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = ArtistSerializer
 
 class StandardResultsSetPagination(pagination.PageNumberPagination):
-    page_size = 100
+    page_size = 50
     page_size_query_param = 'page_size'
     max_page_size = 100
 
@@ -32,22 +31,82 @@ class SongSearchAPIView(generics.ListAPIView):
     pagination_class = StandardResultsSetPagination
 
     def get_queryset(self):
-        query = self.request.query_params.get('query')
-        if not query:
-            raise ValidationError("The 'query' parameter is required.")
+        title = self.request.query_params.get('title')
+        filename = self.request.query_params.get('filename')
+        instrument_text = self.request.query_params.get('instrument_text')
+        comment_text = self.request.query_params.get('comment_text')
+
+        if not any([title, filename, instrument_text, comment_text]):
+            raise ValidationError("At least one of 'title', 'filename', 'instrument_text', or 'comment_text' is required.")
+
+        # Parse min/max parameters
+        min_file_size = self.request.query_params.get('min_file_size')
+        max_file_size = self.request.query_params.get('max_file_size')
+        min_channels = self.request.query_params.get('min_channels')
+        max_channels = self.request.query_params.get('max_channels')
+
+        def parse_positive_int(value):
+            if value is None:
+                return None
+            try:
+                val = int(value)
+                if val <= 0:
+                    return -1  # invalid
+                return val
+            except ValueError:
+                return -1
+
+        min_file_size = parse_positive_int(min_file_size)
+        max_file_size = parse_positive_int(max_file_size)
+        min_channels = parse_positive_int(min_channels)
+        max_channels = parse_positive_int(max_channels)
+
+        if min_file_size == -1 or max_file_size == -1 or min_channels == -1 or max_channels == -1:
+            return Song.objects.none()
+
+        if (min_file_size and max_file_size and min_file_size > max_file_size) or \
+           (min_channels and max_channels and min_channels > max_channels):
+            return Song.objects.none()
 
         file_format = self.request.query_params.get('file_format')
         genre = self.request.query_params.get('genre')
         license_filter = self.request.query_params.get('license')
 
-        queryset = Song.objects.annotate(
-            search=SearchVector('title_vector'),
-        ).filter(
-            search=SearchQuery(query)
-        ).annotate(
-            relevance=F('search')
-        ).order_by('-relevance')
+        queryset = Song.objects.all()
 
+        q_objects = Q()
+        relevance_expr = 0
+
+        if title:
+            search_title = SearchVector('title_vector')
+            rank_title = SearchRank(search_title, SearchQuery(title))
+            queryset = queryset.annotate(search_title=search_title, rank_title=rank_title)
+            q_objects &= Q(search_title=SearchQuery(title))
+            relevance_expr += F('rank_title')
+
+        if instrument_text:
+            search_instrument = SearchVector('instrument_text_vector')
+            rank_instrument = SearchRank(search_instrument, SearchQuery(instrument_text))
+            queryset = queryset.annotate(search_instrument=search_instrument, rank_instrument=rank_instrument)
+            q_objects &= Q(search_instrument=SearchQuery(instrument_text))
+            relevance_expr += F('rank_instrument')
+
+        if comment_text:
+            search_comment = SearchVector('comment_text_vector')
+            rank_comment = SearchRank(search_comment, SearchQuery(comment_text))
+            queryset = queryset.annotate(search_comment=search_comment, rank_comment=rank_comment)
+            q_objects &= Q(search_comment=SearchQuery(comment_text))
+            relevance_expr += F('rank_comment')
+
+        if filename:
+            q_objects &= Q(filename__icontains=filename)
+
+        if q_objects:
+            queryset = queryset.filter(q_objects)
+            if relevance_expr:
+                queryset = queryset.annotate(relevance=relevance_expr).order_by('-relevance')
+
+        # Apply additional filters
         if file_format:
             queryset = queryset.filter(format=file_format)
 
@@ -56,6 +115,18 @@ class SongSearchAPIView(generics.ListAPIView):
 
         if license_filter:
             queryset = queryset.filter(license=license_filter)
+
+        if min_file_size:
+            queryset = queryset.filter(file_size__gte=min_file_size)
+
+        if max_file_size:
+            queryset = queryset.filter(file_size__lte=max_file_size)
+
+        if min_channels:
+            queryset = queryset.filter(channels__gte=min_channels)
+
+        if max_channels:
+            queryset = queryset.filter(channels__lte=max_channels)
 
         return queryset
 
@@ -78,3 +149,29 @@ class SongDownloadView(View):
             response = HttpResponse(file.read(), content_type='application/zip')
             response['Content-Disposition'] = f'attachment; filename="{song.filename}.zip"'
             return response
+
+class GenreListAPIView(generics.ListAPIView):
+    serializer_class = GenreSerializer
+
+    def list(self, request, *args, **kwargs):
+        genres = [{'id': value, 'text': label} for value, label in Song.Genres.choices]
+        serializer = self.get_serializer(genres, many=True)
+        return Response(serializer.data)
+
+class ArtistSearchAPIView(generics.ListAPIView):
+    serializer_class = ArtistSearchResultSerializer
+    pagination_class = StandardResultsSetPagination
+
+    def get_queryset(self):
+        name = self.request.query_params.get('name')
+
+        if not name:
+            raise ValidationError("The 'name' parameter is required.")
+
+        queryset = Artist.objects.annotate(
+            rank=SearchRank('search_document', SearchQuery(name))
+        ).filter(
+            search_document=SearchQuery(name)
+        ).order_by('-rank')
+
+        return queryset
